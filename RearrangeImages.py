61c4e64d2b16c0d4cb142477dedeeb5ca91b1817
Mikkel Loose <38498170+mikkelmedm@users.noreply.github.com>
2018-11-26 14:03:49 +0100

    Update and rename chairfold.py to RearrangeImages.py

--- RearrangeImages.py
+++ RearrangeImages.py
@@ -11,9 +11,10 @@ from keras.utils.generic_utils import CustomObjectScope
 import keras
 import shutil
 
-
+# Works for MobileNet models:
 with CustomObjectScope({'relu6': keras.applications.mobilenet.relu6,'DepthwiseConv2D': keras.applications.mobilenet.DepthwiseConv2D}):
-    model = load_model('mobilechairs.h5')
+    # Load your Keras model here:
+    model = load_model('RetrainedMobileNet.h5')
 
 parser = argparse.ArgumentParser(formatter_class=argparse.ArgumentDefaultsHelpFormatter)
 
@@ -26,15 +27,18 @@ args = parser.parse_args()
 print("I'm working...")
 
 path = args.folder_dir
+# Specify path-name for new directory containing the images you want to move.
 newpath = "Newimgdir"
 dirs = os.listdir( path )
 
 if not os.path.exists(newpath):
     os.makedirs(newpath)
 
+# Names of new folders: 
 goodfolder = newpath+"/"+"good" 
 badfolder = newpath+"/"+"bad" 
 
+#Creating new folders:
 if not os.path.exists(goodfolder):
     os.makedirs(goodfolder)
 if not os.path.exists(badfolder):
@@ -46,7 +50,7 @@ def predictandmove():
     for item in dirs:
         if os.path.isfile(path+"/"+item):
             img_path=path+"/"+item
-            img = cv2.resize(cv2.imread(img_path), (224, 224))
+            img = cv2.resize(cv2.imread(img_path), (224, 224)) # 224,224 is specific for this model.
             x = image.img_to_array(img)
             x = np.expand_dims(x, axis=0)
             x = preprocess_input(x)
@@ -54,6 +58,8 @@ def predictandmove():
             preds = model.predict(x) 
 
             labels = np.argmax(preds, axis=-1)
+            
+            # if a given class, move to a give folder:
             if labels==1:
                 shutil.move(path+"/"+item, goodfolder)
                 count1+=1
